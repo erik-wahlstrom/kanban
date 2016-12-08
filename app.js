63d316db3e19fd6f0bef203b375635dcd9661a18
Erik Wahlstrom <erikwahlstrom@Eriks-MacBook-Pro.local>
2016-12-08 13:19:17 -0800

    Updated password

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 var express = require('express');
 var pg = require('pg');
 
-var conString = 'pg://erik:#Ledville100@aacxnw4546m2a8.caoeqvnamynp.us-west-2.rds.amazonaws.com:5432/kanban';
-//var conString = 'pg://postgres:@localhost:5432/kanban';
+var conString = 'pg://erik:Leadville100@aacxnw4546m2a8.caoeqvnamynp.us-west-2.rds.amazonaws.com:5432/kanban';
+
 var app = express();
 
 app.get('/', function (req, res) {
-    var client = new pg.Client(conString);
-    var conn = client.connect();
+    const results = [];
+    pg.connect(conString, (err, client, done) => {
+        // Handle connection errors
+        if(err) {
+            done();
+            console.log(err);
+            return res.status(500).json({success: false, data: err});
+        }
+        // SQL Query > Select Data
+        const query = client.query('SELECT * FROM state;');
+        // Stream results back one row at a time
+        query.on('row', (row) => {
+            results.push(row);
+        });
+        // After all data is returned, close connection and return results
+        query.on('end', () => {
+            done();
+            return res.json(results);
+        });
+    });
+});
 
-    var query = client.query("select name from state");
 
-  // Stream results back one row at a time
-  query.on('row', (row) => {
-    results.push(row);
-  });
-  // After all data is returned, close connection and return results
-  query.on('end', () => {
-    client.end();
-    return res.json(results);
-  });
- });
-
-
+/*
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+*/
 module.exports = app;
